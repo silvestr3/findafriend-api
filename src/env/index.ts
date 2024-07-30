ba269321df52676ad16226bd750236643ecac2a4
silvestr3 <fellipe.silvestre@outlook.com>
2024-07-30 15:32:20 -0300

    Initial commit

---
+++ src/env/index.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  throw new Error("Invalid environment variables");
+}
+
+export const env = _env.data;
